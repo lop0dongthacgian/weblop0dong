bb0096180ffaade65160ae5d294c584f5cbb0288
lop0dongthacgian <lop0dongthacgian@gmail.com>
2025-06-09 16:57:39 +0700

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,7 @@
+// Mã theo dõi Google Analytics
+// Bạn có thể dễ dàng thay đổi mã ID 'G-X8Z2DSRQLL' tại đây khi cần.
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-X8Z2DSRQLL');
